6c0008728fd5f132e2e26aa4ac6db338fffcbf21
maocurai <76206997+maocurai@users.noreply.github.com>
2022-04-15 16:00:37 +0300

    entities created

--- src/main/java/com/example/tacocloud/domain/Order.java
+++ src/main/java/com/example/tacocloud/domain/Order.java
@@ -4,17 +4,22 @@ import lombok.Data;
 import lombok.ToString;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@ToString
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
@@ -44,9 +49,16 @@ public class Order {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco design) {
         this.tacos.add(design);
     }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
 }
+
--- src/main/java/com/example/tacocloud/domain/Taco.java
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -3,15 +3,18 @@ package com.example.tacocloud.domain;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@ToString
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -20,8 +23,14 @@ public class Taco {
     @Size(min=5, message="Name must be at least 3 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
     public Taco() {}
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
