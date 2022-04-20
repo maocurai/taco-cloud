70c7b8b7a4f82a673e85d69376dcc6db32cce30e
maocurai <76206997+maocurai@users.noreply.github.com>
2022-04-20 18:23:31 +0300

    log out added

--- src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -29,7 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                         .loginPage("/login")
-                        .defaultSuccessUrl("/design");
+                        .defaultSuccessUrl("/design")
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/");
     }
 
     @Override
