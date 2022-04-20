a5799020f64c79d6a62004a482cc17dffb61b08a
maocurai <76206997+maocurai@users.noreply.github.com>
2022-04-20 18:20:31 +0300

    log in added

--- src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                    .antMatchers("/design", "/orders")
+                        .hasRole("ROLE_USER")
+                    .antMatchers("/", "/**").permitAll()
+
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/design");
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
