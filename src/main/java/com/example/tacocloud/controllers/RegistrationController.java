6ebd49bbe8e3c1a7acfe0494346e0ea5d714309b
maocurai <76206997+maocurai@users.noreply.github.com>
2022-04-20 17:45:47 +0300

    application has complete user registration and authentication support

--- src/main/java/com/example/tacocloud/controllers/RegistrationController.java
+++ src/main/java/com/example/tacocloud/controllers/RegistrationController.java
@@ -1,4 +1,39 @@
 package com.example.tacocloud.controllers;
 
+import com.example.tacocloud.domain.RegistrationForm;
+import com.example.tacocloud.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
 public class RegistrationController {
+
+    private UserRepository userRepo;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationController(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepo.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
 }
